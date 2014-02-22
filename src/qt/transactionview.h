c2222897dc01dba49b8324b0d438cd3a937ec0d3
ROBOKiTTY <stillwatersca@gmail.com>
2014-02-22 01:32:14 -0800

    KiTTYCoin

--- src/qt/transactionview.h
+++ src/qt/transactionview.h
@@ -67,7 +67,7 @@ private slots:
     void copyLabel();
     void copyAmount();
     void copyTxID();
-    void viewOnDogechain();
+    void viewOnKiTTYchain();
 
 signals:
     void doubleClicked(const QModelIndex&);
