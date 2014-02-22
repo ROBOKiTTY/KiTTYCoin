5b9ebf83d53fe0c6606a9c69ab778981ec06ae2e
ROBOKiTTY <stillwatersca@gmail.com>
2014-02-21 21:44:00 -0800

    KiTTYCoin

--- src/net.cpp
+++ src/net.cpp
@@ -1113,7 +1113,7 @@ void ThreadMapPort()
             }
         }
 
-        string strDesc = "Dogecoin " + FormatFullVersion();
+        string strDesc = "KiTTYCoin " + FormatFullVersion();
 
         try {
             loop {
@@ -1680,7 +1680,7 @@ bool BindListenPort(const CService &addrBind, string& strError)
     {
         int nErr = WSAGetLastError();
         if (nErr == WSAEADDRINUSE)
-            strError = strprintf(_("Unable to bind to %s on this computer. Dogecoin is probably already running."), addrBind.ToString().c_str());
+            strError = strprintf(_("Unable to bind to %s on this computer. KiTTYCoin is probably already running."), addrBind.ToString().c_str());
         else
             strError = strprintf(_("Unable to bind to %s on this computer (bind returned error %d, %s)"), addrBind.ToString().c_str(), nErr, strerror(nErr));
         printf("%s\n", strError.c_str());
